96d973c6c82d27bcd68bf92cf8d5d17a2d83e9a3
alvinkenyagah <alvin47k@gmail.com>
2023-05-02 20:35:45 +0300

    Create Home About Contact components

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import  React from 'react'
+
+export default function NavBar(){
+
+
+
+
+return(
+
+
+<h2>NavBar</h2>
+
+
+
+)
+
+
+
+
+
+
+}
---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home()
+{
+
+
+
+
+return(
+
+
+    <h1>HOME</h1>
+)
+
+
+
+
+}
