5429db68e27893ac271aec2bc895c1783fab579c
alvinkenyagah <alvin47k@gmail.com>
2023-05-03 09:35:57 +0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/home";
 import ContactForm from "./components/ContactUs";
+// import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
     <div>
+  
       <Home />
       <div className="App">
         <ContactForm />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import  React from 'react'
-
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 export default function NavBar(){
 
 
@@ -7,9 +8,12 @@ export default function NavBar(){
 
 return(
 
-
-<h2>NavBar</h2>
-
+<BrowserRouter>
+<Route path="/" element={<home/>}/>
+<Route path="search" element={<Search/>}/>
+<Route path="contact" element={<>}/>
+<Route path="/" element={<home/>}/>
+</BrowserRouter>
 
 
 )
