e6c16ca125c2611b446d4b3a3dc36c7b33e6e213
alvinkenyagah <alvin47k@gmail.com>
2023-05-03 10:39:11 +0300

    Add NavBar component and set up Routing to individual pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Search from "./components/Search";
 import About from "./components/AboutUs";
@@ -8,12 +8,28 @@ import ContactForm from "./components/ContactUs";
 
 function App() {
   return (
+
+    <>
+    <nav>
+
+    <ul style={{display:"flex",gap:"5%",listStyle:"none"}}>
+  <li><Link to="/">Home</Link></li>
+  <li><Link to="/search">Search</Link></li>
+  <li><Link to="/about">About</Link></li>
+  <li><Link to="/contact">Contact</Link></li>
+
+    </ul>
+
+    </nav>
+
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<ContactForm />} />
     </Routes>
+    </>
   );
 }
 
