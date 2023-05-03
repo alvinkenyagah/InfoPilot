52c59507722d2739ff55ba054f7357616e0eaebb
alvinkenyagah <alvin47k@gmail.com>
2023-05-03 14:02:02 +0300

    Center navigaaion bar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,47 @@ import Home from "./components/Home";
 import Search from "./components/Search";
 import About from "./components/AboutUs";
 import ContactForm from "./components/ContactUs";
+import React, { useState } from "react";
+
 
 function App() {
+
+  
+    const [isDarkMode, setIsDarkMode] = useState(false);
+  
+    const handleClick = () => {
+      setIsDarkMode(!isDarkMode);
+    };
+  
+
+
+
   return (
 
     <>
+    <div className="hero">
     <nav>
-
-    <ul style={{display:"flex",gap:"5%",listStyle:"none"}}>
+    <img src="https://img.icons8.com/dusk/64/null/geography--v1.png" className="logo"/>
+    <ul>
   <li><Link to="/">Home</Link></li>
   <li><Link to="/search">Search</Link></li>
   <li><Link to="/about">About</Link></li>
   <li><Link to="/contact">Contact</Link></li>
 
     </ul>
+    <button
+      className="dark"
+      onClick={handleClick}
+      style={{
+        backgroundColor: isDarkMode ? "black" : "white",
+        color: isDarkMode ? "white" : "black",
+      }}
+    >
+      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    </button>
 
     </nav>
+    </div>
 
 
     <Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
+import "./Home.css";
+export default function Home() {
+  return (
+    <>
+      <div className="heading">
+        <p>Welcome <br/> to InfoPilot</p>
+        
+      </div>
 
-export default function Home()
-{
-
-
-
-
-return(
-
-
-    <h1 style={{textAlign:"center"}}>HOME</h1>
-)
-
-
-
-
+      <div className="summary">
+        <p>Your source for interesting topics</p>
+        <p>and latest information around the world. </p>
+      </div>
+    </>
+  );
 }
