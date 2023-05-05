a7445dad450e5772dd6c7b5b8c7bc0e7c6625e5d
alvinkenyagah <alvin47k@gmail.com>
2023-05-05 11:27:26 +0300

    Change dropdown value of sports from sports to sport

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,3 +1,170 @@
+// import React, { useState } from "react";
+// import "./search.css";
+
+// function Search() {
+//   const [articles, setArticles] = useState([]);
+//   const [searchQuery, setSearchQuery] = useState("");
+
+//   const [language, setLanguage] = useState("");
+//   const [topic, setTopic] = useState("");
+//   const [date, setDate] = useState("");
+//   const [country, setCountry] = useState("");
+
+//   const handleSearchChange = (event) => {
+//     setSearchQuery(event.target.value);
+//   };
+
+//   const handleLanguageChange = (event) => {
+//     setLanguage(event.target.value);
+//   };
+
+//   const handleTopicChange = (event) => {
+//     setTopic(event.target.value);
+//   };
+
+//   const handleDateChange = (event) => {
+//     setDate(event.target.value);
+//   };
+
+//   const handleCountryChange = (event) => {
+//     setCountry(event.target.value);
+//   };
+
+//   const handleSearchSubmit = (event) => {
+//     event.preventDefault();
+//     let url = `https://api.newscatcherapi.com/v2/search?q=${searchQuery}`
+//     if (topic) url += `&topic=${topic}`
+//     if (language) url += `&lang=${language}`
+//     if (date) url += `&date=${date}`
+//     if (country) url += `&country=${country}`
+
+//     console.log(language)
+//     console.log(topic)
+
+//     fetch(url, {
+//       method: "GET",
+//       headers: {
+//         "x-api-key": "fQNAKTTdUmsQ7rEA_CqkRUHnDU5chzsCw_AvUuCt2-M",
+//         "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setArticles(data.articles);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   // topic options
+
+//   const topicOptions = [
+//     { label: "news", value: "news" }, 
+//     { label: "sports", value: "sports" },
+//     { label: "technology", value: "tech" },
+//     { label: "world", value: "world" },
+//     { label: "finance", value: "finance" },
+//     { label: "politics", value: "politics" },
+//     { label: "business", value: "business" },
+//     { label: "economics", value: "economics" },
+//     { label: "entertainment", value: "entertainment" },
+//     { label: "beauty", value: "beauty" },
+//     { label: "travel", value: "travel" },
+//     { label: "music", value: "music" },
+//     { label: "food", value: "food" },
+//     { label: "science", value: "science" },
+//     { label: "gaming", value: "gaming" },
+//     { label: "energy", value: "energy" },
+//   ];
+
+//   // language options
+//   const languageOptions = [
+//     { label: "English", value: "en" },
+//     { label: "Spanish", value: "es" },
+//     { label: "French", value: "fr" },
+//     { label: "German", value: "de" },
+//     { label: "Italian", value: "it" },
+//     { label: "Russian", value: "ru" },
+//   ];
+
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSearchSubmit}>
+//         <div id="main-search">
+//           <label>
+//             <input
+//               className="input"
+//               type="text"
+//               placeholder="Search for something"
+//               value={searchQuery}
+//               onChange={handleSearchChange}
+//             />
+//           </label>
+//           <button type="submit" className="button-85">
+//             Search
+//           </button>
+//         </div>
+//         <br />
+//         <br />
+
+//         <h3>Advanced search</h3>
+//         <label>
+//           Topic:
+//           <select value={language}  onChange={handleTopicChange}>
+//             <option value="English">select topic</option>
+//             {topicOptions.map((option) => (
+//               <option key={option.value} value={option.value}>
+//                 {option.label}
+//               </option>
+//             ))}
+//           </select>
+//         </label>
+//         <br />
+//         <label>
+//           Language:
+//           <select value={topic} onChange={handleLanguageChange}>
+//             <option value="English">select language</option>
+//             {languageOptions.map((option) => (
+//               <option key={option.value} value={option.value}>
+//                 {option.label}
+//               </option>
+//             ))}
+//           </select>
+//         </label>
+//         <br />
+//         <label>
+//           Date:
+//           <input type="text" value={date} onChange={handleDateChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Country:
+//           <input type="text" value={country} onChange={handleCountryChange} />
+//         </label>
+//         <br />
+//         <button type="submit">Search</button>
+//       </form>
+
+//       {articles.length > 0 &&
+//   articles.map((article) => (
+//     <div key={article._id}>
+//       <img src={article.media} alt="2" style={{ width: "200px" }} />
+//       <h2>{article.title}</h2>
+//       <p>{article.summary}</p>
+//       <a href={article.link}>Read more</a>
+//     </div>
+//   ))
+// }
+
+//     </div>
+//   );
+// }
+
+// export default Search;
+
+
 import React, { useState } from "react";
 import "./search.css";
 
@@ -58,7 +225,7 @@ function Search() {
 
   const topicOptions = [
     { label: "news", value: "news" }, // added value to the first option
-    { label: "sports", value: "sports" },
+    { label: "sports", value: "sport" },
     { label: "technology", value: "tech" },
     { label: "world", value: "world" },
     { label: "finance", value: "finance" },
@@ -91,21 +258,25 @@ function Search() {
         onSubmit={handleSearchSubmit}
         style={{ marginLeft: "3%", marginTop: "5%", fontSize: "1.3rem" }}
       >
-        <label>
-          Search query:
-          <input
-        className="textInput"
-            type="text"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          />
-        </label>
-        <button type="submit">Search</button>
+       <div id="main-search">
+           <label>
+             <input
+               className="input"
+               type="text"
+               placeholder="Search for something"
+               value={searchQuery}
+               onChange={handleSearchChange}
+             />
+           </label>
+           <button type="submit" className="button-85">
+             Search
+           </button>
+         </div>
 <br/>
 <br/>
         <label>
           Topic:
-          <select value={language} onChange={handleLanguageChange}>
+          <select value={topic} onChange={handleTopicChange}>
             <option value="English">select topic</option>
             {topicOptions.map((option) => (
               <option key={option.value} value={option.value}>
@@ -117,7 +288,7 @@ function Search() {
 
         <label>
           Language:
-          <select value={topic} onChange={handleTopicChange}>
+          <select value={language} onChange={handleLanguageChange}>
             <option value="English">select language</option>
             {languageOptions.map((option) => (
               <option key={option.value} value={option.value}>
